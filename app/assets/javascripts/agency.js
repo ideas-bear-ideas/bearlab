9c80d0767e509dbd5f3ed9aa7bbe03291c85cdd6
Surim Kim <kimsuelim@gmail.com>
2017-07-05 23:18:09 +0900

    reformat

--- app/assets/javascripts/agency.js
+++ app/assets/javascripts/agency.js
@@ -1,29 +1,29 @@
 // Agency Theme JavaScript
-$(document).ready(function () {
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').on('click', function(event) {
-        event.preventDefault();
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-    });
+$(document).ready(function() {
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('a.page-scroll').on('click', function(event) {
+    event.preventDefault();
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+  });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 51
+  });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
-    });
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function(){
+    $('.navbar-toggle:visible').click();
+  });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+  // Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 100
+    }
+  })
 });
